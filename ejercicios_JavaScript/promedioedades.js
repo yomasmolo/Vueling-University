616252e6649bf5254b64af3db64b51cf070c8dd1
Marc Ruiz <marc.ruiz@col.vueling.com>
2023-05-24 14:38:09 +0200

    Mejora ejercicio promedioedades

--- ejercicios_JavaScript/promedioedades.js
+++ ejercicios_JavaScript/promedioedades.js
@@ -1,16 +1,21 @@
+let marc = { Nombre: "Marc", edad: 25 };
+let nacho = { Nombre: "Nacho", edad: 30 };
+let raul = { Nombre: "Raul", edad: 29 };
+let adri = { Nombre: "Adri", edad: 17 };
+let arr = [ marc, nacho, raul, adri];
+console.log(arr);
+
 function obtenerPromedio(users) {
-    let suma = 0;
-    for (let i = 0; i < users.length; i++) {
-      suma = suma + users[i].edad; 
-    }
-    return suma / users.length;
+  let suma = 0;
+  let mayores = [];
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].edad >= 18) {
+      mayores.push(users[i].edad);
+    } 
   }
-  
-  let marc = { Nombre: "John", edad: 25 };
-  let nacho = { Nombre: "Pete", edad: 30 };
-  let raul = { Nombre: "Mary", edad: 29 };
-  
-  let arr = [ marc, nacho, raul ];
-  
-  console.log(arr);
-  console.log(obtenerPromedio(arr));
+  console.log(mayores);
+  mayores.forEach(function(a){suma += a;});
+  return suma / mayores.length;
+}
+
+console.log(obtenerPromedio(arr));
