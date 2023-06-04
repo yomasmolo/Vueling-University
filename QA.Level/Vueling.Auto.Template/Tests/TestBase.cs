7def7c0c063a122fd10e302d6163c8aae4de605a
Marc Ruiz <marc.ruiz@col.vueling.com>
2023-06-05 01:30:57 +0200

    Autos Level actualizadas

--- QA.Level/Vueling.Auto.Template/Tests/TestBase.cs
+++ QA.Level/Vueling.Auto.Template/Tests/TestBase.cs
@@ -107,7 +107,7 @@ namespace Level.Auto.Tests
             test.Log(logstatus, "Test ended with " + logstatus + " Message: " + message + "<br/>" + stackTrace + "<br/><br/>", MediaEntityBuilder.CreateScreenCaptureFromPath(page.TakeScreenshot(SetUpFixtureBase.HTMLPath)).Build());
 
             page.TakeScreenshot(SetUpFixtureBase.HTMLPath);
-            //page.CloseDriver();
+            page.CloseDriver();
             Thread.Sleep(1000);
         }
     }
